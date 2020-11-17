63c96640b090a13383b8658126797cb51c8581c6
Adam Wathan <adam.wathan@gmail.com>
2020-11-17 08:48:41 -0500

    Remove aggressive border override

--- src/index.js
+++ src/index.js
@@ -128,7 +128,6 @@ const forms = plugin(function ({ addBase, theme }) {
       '--tw-ring-offset-shadow': `var(--tw-ring-inset) 0 0 0 var(--tw-ring-offset-width) var(--tw-ring-offset-color)`,
       '--tw-ring-shadow': `var(--tw-ring-inset) 0 0 0 calc(2px + var(--tw-ring-offset-width)) var(--tw-ring-color)`,
       'box-shadow': `var(--tw-ring-offset-shadow), var(--tw-ring-shadow), var(--tw-shadow, 0 0 #0000)`,
-      'border-color': theme('colors.gray.500', colors.gray[500]),
     },
 
     [`
