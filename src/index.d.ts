1336243b4f616e79b7b929cb7b8a7ffe0a5338d3
Robin Malfait <malfait.robin@gmail.com>
2022-05-18 15:26:13 +0200

    update types
    - This way it doesn't rely on the `@types/tailwindcss` Definetely Typed
      repo which could cause issues when we release Tailwind CSS 3.1 with
      our own types.
    - Use `export = plugin` instead of `export default plugin` so that it is
      compatible with `module.export`

--- src/index.d.ts
+++ src/index.d.ts
@@ -1,4 +1,2 @@
-import { TailwindPluginWithOptionsFn } from 'tailwindcss/plugin'
-
-declare const plugin: TailwindPluginWithOptionsFn<{ strategy?: 'base' | 'class' }>
-export default plugin
+declare function plugin(options?: { strategy?: 'base' | 'class' }): Function
+export = plugin
