07af0555a757fe45d67fd1e25757e12801340ab6
Karl Horky <karl.horky@gmail.com>
2023-07-13 14:32:13 +0200

    Allow for <input> without `type`

--- src/index.js
+++ src/index.js
@@ -17,6 +17,7 @@ const forms = plugin.withOptions(function (options = { strategy: undefined }) {
       {
         base: [
           "[type='text']",
+          ":not([type])",
           "[type='email']",
           "[type='url']",
           "[type='password']",
